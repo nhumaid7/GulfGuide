d778fc3c7bf3d2f8eb6c35c2c906ddf5adb13715
Batool Albonni <batool.m52020@gmail.com>
2026-04-09 19:35:11 +0300

    add color, font, icon, and bootstrap css

--- PhpProject/index.php
+++ PhpProject/index.php
@@ -4,11 +4,28 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to c
 Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>GulfGuide</title>
-    </head>
-    <body>
-        <h1>Welcome to GulfGuid</h1>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>GulfGuide</title>
+    <!-- phosphor icons -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css" />
+
+    <!-- bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- custom CSS -->
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
+</head>
+
+<body>
+    <main class="main">
+        <h1>Welcome to GulfGuide</h1>
+    </main>
+</body>
+
 </html>
