8042e5135f8c9b5ab5ad0d7b6fd7a110224d044f
nhumaid7 <202301706@student.polytechnic.bh>
2026-04-22 12:19:06 +0300

    testing

--- PhpProject/config/dbConn.php
+++ PhpProject/config/dbConn.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_SERVER',   '20.74.143.233'); 
-define('DB_USERNAME', 'u202304056');
+define('DB_SERVER',   'localhost'); 
+define('DB_USERNAME', 'u202301706');
 define('DB_PASSWORD', "asdASD123!");
 define('DB_NAME', 'db202304056');
 
